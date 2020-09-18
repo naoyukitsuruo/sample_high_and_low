b2c6533342f10d2992c31630df5eefeede5900fc
Naoyuki Tsuruo <naoyukitsuruo@NaoyukinoMacBook-Pro.local>
2020-09-18 17:07:50 +0900

    プログラム作成

--- app/src/main/java/com/example/highandlow/MainActivity.kt
+++ app/src/main/java/com/example/highandlow/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.highandlow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "high and low"
+    private var yourCard = 0
+    private var droidCard = 0
+    private var hitCount = 0
+    private var loseCount = 0
+    private var gameStart = false
+    private var answered = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        hitCount = 0
+        loseCount = 0
+        hitText
+    }
+
+    private fun drawCard(){
+
+        yourCardImage.setImageResource(R.drawable.z02)
+        droidCardImage.setImageResource(R.drawable.z01)
+
+        yourCard = (1..13).random()
+        Log.d(tag, "You:"+yourCard)
+
+        when(yourCard) {
+            1 -> yourCardImage.setImageResource(R.drawable.d01)
+            2 -> yourCardImage.setImageResource(R.drawable.d02)
+            3 -> yourCardImage.setImageResource(R.drawable.d03)
+            4 -> yourCardImage.setImageResource(R.drawable.d04)
+            5 -> yourCardImage.setImageResource(R.drawable.d05)
+            6 -> yourCardImage.setImageResource(R.drawable.d06)
+            7 -> yourCardImage.setImageResource(R.drawable.d07)
+            8 -> yourCardImage.setImageResource(R.drawable.d08)
+            9 -> yourCardImage.setImageResource(R.drawable.d09)
+            10 -> yourCardImage.setImageResource(R.drawable.d10)
+            11 -> yourCardImage.setImageResource(R.drawable.d11)
+            12 -> yourCardImage.setImageResource(R.drawable.d12)
+            13 -> yourCardImage.setImageResource(R.drawable.d13)
+        }
+
+        droidCard = (1..13).random()
+        Log.d(tag, "droid:"+droidCard)
+        answered = false
+
     }
 }
